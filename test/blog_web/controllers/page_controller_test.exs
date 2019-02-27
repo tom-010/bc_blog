9fad3fbc2e183c077874075fdf390620101737e6
Thomas Deniffel <tdeniffel@acm.org>
2019-02-27 13:05:38 +0100

    working

--- test/blog_web/controllers/page_controller_test.exs
+++ test/blog_web/controllers/page_controller_test.exs
@@ -1,8 +1,13 @@
 defmodule BlogWeb.PageControllerTest do
   use BlogWeb.ConnCase
 
-  test "GET /", %{conn: _conn} do
-    # conn = get(conn, "/")
-    # assert html_response(conn, 200) =~ "Welcome to BC!"
+  test "Brutal Coding in Title", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "<title>Brutal Coding"
+  end
+
+  test "links to articles", %{conn: conn} do
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "href=\"/article/thank-you-for-the-big-effort-of-writing-such-an-extensive-answer_\""
   end
 end
