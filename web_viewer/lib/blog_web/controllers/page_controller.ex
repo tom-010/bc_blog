dbb61d8fdc34ee916fd1c5c9dd4c2c20e1403dd6
Thomas Deniffel <tdeniffel@acm.org>
2019-03-14 11:13:49 +0100

    working

--- web_viewer/lib/blog_web/controllers/page_controller.ex
+++ web_viewer/lib/blog_web/controllers/page_controller.ex
@@ -5,6 +5,18 @@ defmodule BlogWeb.PageController do
     render(conn, "index.html")
   end
 
+  def articles(conn, _params) do
+    render(conn, "articles.html", articles: get_articles())
+  end
+
+  def article(conn, %{"slug" => slug}) do 
+    [article | _] =
+      get_articles()
+      |> Enum.filter(& slug == &1.slug)
+
+    render(conn, "article.html", article: article)
+  end
+
   defp get_articles() do 
     "articles"
     |> ArticleReader.read()
@@ -12,15 +24,4 @@ defmodule BlogWeb.PageController do
     |> Enum.map(&HtmlArticle.from_article/1)
   end
 
-  def articles(conn, _params) do
-    render(conn, "articles.html", articles: get_articles())
-  end
-
-  def article(conn, %{"slug" => slug}) do 
-    [article | _] =
-    get_articles()
-    |> Enum.filter(& slug == &1.slug)
-
-    render(conn, "article.html", article: article)
-  end
 end
