2f937105f53e36fa0513aa633f147596f1cdd2d3
Thomas Deniffel <tdeniffel@acm.org>
2019-03-14 12:10:15 +0100

    working

--- cached_article_repo/lib/cached_article_repo.ex
+++ cached_article_repo/lib/cached_article_repo.ex
@@ -1,8 +1,13 @@
 defmodule CachedArticleRepo do
   
   @path_to_posts "../blog_post_res"
-  
-  def hello do
-    :world
+
+  defp get_articles() do 
+    @path_to_posts
+    |> ArticleReader.read()
+    |> Enum.map(&Article.from_article_file/1)
+    |> Enum.map(&HtmlArticle.from_article/1)
   end
+
+  
 end
