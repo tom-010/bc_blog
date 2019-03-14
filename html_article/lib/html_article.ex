a30f86752cb3839c0aa9eaf4549cf89caa722037
Thomas Deniffel <tdeniffel@acm.org>
2019-03-14 10:55:13 +0100

    working

--- html_article/lib/html_article.ex
+++ html_article/lib/html_article.ex
@@ -3,6 +3,6 @@ defmodule HtmlArticle do
   def from_article(article) do 
     r = %{article | 
       content: MarkdownHtmlConverter.convert(article.content)}
-    
+    r = Map.put(r, :author, "")
   end
 end
