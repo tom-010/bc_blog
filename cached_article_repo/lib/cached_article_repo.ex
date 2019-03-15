79cc53f8d8dcc78f22ac146556f6bde35675ea2b
Thomas Deniffel <tdeniffel@acm.org>
2019-03-15 21:31:09 +0100

    Genserver as cache

--- cached_article_repo/lib/cached_article_repo.ex
+++ cached_article_repo/lib/cached_article_repo.ex
@@ -1,22 +1,106 @@
 defmodule CachedArticleRepo do
-  
-  @blog_post_default "../blog_post_res"
+  use GenServer
 
-  def get_articles() do 
+  def get_articles() do
+    GenServer.call(:article_cache, :get_messages)
+  end
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: :article_cache)
+  end
+
+  def refresh() do 
+    read_articles() |> set_articles()
+  end
+
+  def clear() do 
+    set_articles([])
+  end
+
+  defp read_articles() do 
     with_default(System.get_env("ARTICLE_PATH"), @blog_post_default)
-    |> IO.inspect
     |> ArticleReader.read()
     |> Enum.map(&Article.from_article_file/1)
     |> Enum.map(&HtmlArticle.from_article/1)
+  end
+
+  defp set_articles(articles) do
+    GenServer.cast(:article_cache, {:add_message, articles})
   end
 
   defp with_default(nil, default) do 
     default
   end
 
-  defp with_default(str, default) do 
+  defp with_default(str, _default) do 
     str
   end
 
+  # SERVER
 
+  def init(messages) do
+    {:ok, messages}
+  end
+
+  def handle_cast({:add_message, new_articles}, _articles) do
+    {:noreply, new_articles}
+  end
+
+  def handle_call(:get_messages, _from, messages) do
+    {:reply, messages, messages}
+  end
 end
+
+# defmodule CachedArticleRepo do
+#   use GenServer
+
+#   @blog_post_default "../blog_post_res"
+
+#   def start_link() do
+#     GenServer.start_link(__MODULE__, name: :article_cache)
+#     # refresh()
+#   end
+
+#   def refresh() do
+#     GenServer.call(:article_cache, {:set, "abc"})
+#   end
+
+#   def get_articles() do
+#     {:reply, articles} = GenServer.call(:article_cache, :get)
+#     articles
+
+#   end
+
+
+#   defp read_articles() do 
+#     with_default(System.get_env("ARTICLE_PATH"), @blog_post_default)
+#     |> ArticleReader.read()
+#     |> Enum.map(&Article.from_article_file/1)
+#     |> Enum.map(&HtmlArticle.from_article/1)
+#   end
+
+#   defp with_default(nil, default) do 
+#     default
+#   end
+
+#   defp with_default(str, default) do 
+#     str
+#   end
+
+#   @impl true
+#   def init(stack) do
+#     {:ok, stack}
+#   end
+
+#   @impl true
+#   def handle_call(:get, _from, articles) do
+#     {:reply, articles}
+#   end
+
+#   @impl true
+#   def handle_cast({:set, articles}, state) do
+#     {:noreply, articles}
+#   end
+
+
+# end
--- cached_article_repo/test/cached_article_repo_test.exs
+++ cached_article_repo/test/cached_article_repo_test.exs
@@ -2,5 +2,90 @@ defmodule CachedArticleRepoTest do
   use ExUnit.Case
   doctest CachedArticleRepo
 
+  @test_dir "test/res"
+
+  setup do
+    System.put_env("ARTICLE_PATH", @test_dir)
+    prepare_dirs()
+    CachedArticleRepo.start_link()
+    on_exit fn -> 
+      CachedArticleRepo.clear()
+      clean() 
+    end
+    :ok
+  end
+
+  test "non-existing path empty article-list" do 
+    System.put_env("ARTICLE_PATH", "this/path/does/not/exist")
+    assert CachedArticleRepo.get_articles() == []
+  end
+
+  test "cached article repo" do 
+    create_article("some article")
+    CachedArticleRepo.refresh()
+    articles = CachedArticleRepo.get_articles()
+    assert Enum.count(articles) == 1
+    assert Enum.at(articles, 0).title == "some article"
+  end
+
+  test "multiple articles in dir" do 
+    create_article("one article")
+    create_article("other article")
+    CachedArticleRepo.refresh()
+    articles = CachedArticleRepo.get_articles()
+    assert Enum.count(articles) == 2
+  end
+
+  test "without cache-busting, the article returned gets not updated" do 
+    create_article("some article")
+    CachedArticleRepo.refresh()
+    articles = CachedArticleRepo.get_articles()
+    update_aricle("some article", "new content")
+    new_articles = CachedArticleRepo.get_articles()
+    assert articles == new_articles # unchanged    
+  end
+
+  test "with cache-busting, updated article ist returned" do 
+    create_article("some article")
+    CachedArticleRepo.refresh()
+    update_aricle("some article", "new content")
+    CachedArticleRepo.refresh()
+    new_articles = CachedArticleRepo.get_articles()
+    assert Enum.at(new_articles, 0).content =~ "new content"
+  end 
+
+  defp update_aricle(title, content) do 
+    create_article(title, content);
+  end
+
+  defp create_article(title) do 
+    create_article(title, title)
+  end
+
+  defp create_article(title, content) do 
+    File.write("#{@test_dir}/category1/#{title}.md", """
+    #{title}
+    ========
+
+    |info  |               |
+    |------|---------------|
+    |date  |01.01.2019     |
+    |author|Thomas Deniffel|
+    |key 1 | val 1         |
+    |tags  |tag1,tag2      |
+
+    #{content}
+    """)
+  end
+
+  defp prepare_dirs() do 
+    File.mkdir("#{@test_dir}")
+    File.mkdir("#{@test_dir}/category1")
+  end
+
+  defp clean() do 
+    File.rm_rf(@test_dir)
+  end
   
+
 end
